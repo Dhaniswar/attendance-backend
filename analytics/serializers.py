019a7a83dc068f7e3dc1501226c14d2bcad6ecf2
Anonymous <bkdhaniswar7991@gmail.com>
2026-01-10 16:09:40 +0100

    feat<reports>:import and export

--- analytics/serializers.py
+++ analytics/serializers.py
@@ -9,3 +9,14 @@ class StatisticsSerializer(serializers.Serializer):
     attendance_rate_today = serializers.FloatField()
     average_attendance_rate = serializers.FloatField()
     recent_activity = serializers.ListField(child=serializers.DictField())
+    
+    
+
+
+
+class ExportSerializer(serializers.Serializer):
+    start_date = serializers.DateField(required=True)
+    end_date = serializers.DateField(required=True)
+    format = serializers.ChoiceField(choices=['excel', 'csv', 'pdf'], default='excel')
+    include_details = serializers.BooleanField(default=False)
+
