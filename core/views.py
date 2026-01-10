d600715df3d7d9ae6d3ebb87a7c15516e330e034
Anonymous <bkdhaniswar7991@gmail.com>
2026-01-10 15:50:30 +0100

    feat<healt-checker>: Create API fore health cheacker

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 
